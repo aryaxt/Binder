f28a872ab128b893ab26c0e95d311fb90fb19920
Aryan Ghassemi <aryaxt@gmail.com>
2016-05-01 16:55:22 -0700

    writing tests

---
+++ BinderTests/UIControl+BinderTests.swift
@@ -0,0 +1,37 @@
+//
+//  BinderTests.swift
+//  BinderTests
+//
+//  Created by Aryan Ghassemi on 5/1/16.
+//  Copyright © 2016 Aryan Ghassemi. All rights reserved.
+//
+
+import XCTest
+@testable import Binder
+
+class UIControlBinderTests: XCTestCase {
+	
+	func testTouchDown() { assertControlEvent(.TouchDown) }
+	func testTouchDownRepeat() { assertControlEvent(.TouchDownRepeat) }
+	func testTouchDragInside() { assertControlEvent(.TouchDragInside) }
+	func testTouchDragEnter() { assertControlEvent(.TouchDragEnter) }
+	func testTouchDragExit() { assertControlEvent(.TouchDragExit) }
+	func testTouchUpInside() { assertControlEvent(.TouchUpInside) }
+	func testTouchUpOutside() { assertControlEvent(.TouchUpOutside) }
+	func testValueChanged() { assertControlEvent(.ValueChanged) }
+	func testEditingDidBegin() { assertControlEvent(.EditingDidBegin) }
+	func testEditingChanged() { assertControlEvent(.EditingChanged) }
+	func testEditingDidEnd() { assertControlEvent(.EditingDidEnd) }
+	func testEditingDidEndOnExit() { assertControlEvent(.EditingDidEndOnExit) }
+	func testAllTouchEvents() { assertControlEvent(.AllTouchEvents) }
+	func testAllEditingEvents() { assertControlEvent(.AllEditingEvents) }
+	
+	private func assertControlEvent(event: UIControlEvents) {
+		let button = UIButton()
+		var eventDetected = false
+		button.bindControlEvent(event) { eventDetected = true }
+		button.sendActionsForControlEvents(event)
+		XCTAssert(eventDetected == true)
+	}
+    
+}
